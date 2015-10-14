bf8f8f7fbbe1bad9b6ae87307c70631798c6fb78
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-13 21:54:56 -0700

    add extra gulp, working api request

--- src/app/blogs/blog.ctrl.js
+++ src/app/blogs/blog.ctrl.js
@@ -10,7 +10,20 @@ angular.module("blogapp").controller("BlogCtrl", ["BlogsService", "$routeParams"
     BlogsService
       .get($routeParams.blog_id)
       .then(function (resp) {
-        vm.blog = resp.data;
+        // vm.blog = resp.data;
+
+        console.log(resp);
+
+        vm.blog = {};
+        vm.blog.content = '';
+
+        for (files in resp.files) {
+          vm.blog.content += resp.files[file].content;
+        }
+
+        vm.blog.date = resp.updated_at;
+        vm.blog.author = resp.owner.login;
+
       });
   }
 }]);
