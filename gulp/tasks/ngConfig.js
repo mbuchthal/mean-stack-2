18a5ddc6eebb6bad03cfc796fea5c9450c38d2bd
Benton Green <bentongreen076@gmail.com>
2015-10-15 11:09:16 -0700

    Search bar functionality completed

--- gulp/tasks/ngConfig.js
+++ gulp/tasks/ngConfig.js
@@ -7,8 +7,8 @@ gulp.task("ngConfig", function() {
   var tokenFile = config.dest  + "/token.txt";  // !!! .txt?
 
   // Create a temp file with the token stored in it
-  //fs.writeFileSync(tokenFile, '{"token": "' + process.env.GITHUBTOKEN + '"}');
-  fs.writeFileSync(tokenFile, '{"token": "' + "" + '"}');
+  fs.writeFileSync(tokenFile, '{"token": "' + process.env.GITHUBTOKEN + '"}');
+  //fs.writeFileSync(tokenFile, '{"token": "' + "" + '"}');
 
   // Generate the token config file
   gulp.src(tokenFile)
