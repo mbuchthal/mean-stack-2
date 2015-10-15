c5562e67916154755e7936b2fc84035db48406e9
Benton Green <bentongreen076@gmail.com>
2015-10-15 10:15:47 -0700

    end of branch work

--- gulp/tasks/ngConfig.js
+++ gulp/tasks/ngConfig.js
@@ -7,7 +7,8 @@ gulp.task("ngConfig", function() {
   var tokenFile = config.dest  + "/token.txt";  // !!! .txt?
 
   // Create a temp file with the token stored in it
-  fs.writeFileSync(tokenFile, '{"token": "' + process.env.GITHUBTOKEN + '"}');
+  //fs.writeFileSync(tokenFile, '{"token": "' + process.env.GITHUBTOKEN + '"}');
+  fs.writeFileSync(tokenFile, '{"token": "' + "c5e7199d1f6fd489c2e5fb841e6dd4269484eb6d" + '"}');
 
   // Generate the token config file
   gulp.src(tokenFile)
